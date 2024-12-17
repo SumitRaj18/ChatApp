8bf2c210583f90dcb846fe1d3d55e659480d53ba
SumitRaj18 <sumitraj7162@gmail.com>
2024-12-17 22:35:38 +0530

    update

--- backend/frontend/src/home/Mobile.jsx
+++ backend/frontend/src/home/Mobile.jsx
@@ -23,16 +23,18 @@ const Mobile = () => {
     <div>
         <div style={{backgroundImage:`url(${image})`,backgroundRepeat:'no-repeat',backgroundSize:'cover'}} className=' w-full  text-white' >
     {!selectedConvo?  <div  className='left h-screen'> <Left/> </div>  : ( <>
-     <div className='h-screen w-full overflow-hidden'>
-      <div className='h-[20vh] mt-20 flex w-full bg-slate-950'> <GoArrowLeft onClick={refreshPage} className='text-4xl ml-1  mt-20 bg-slate-950' /> 
+     <div className='h-full w-full overflow-hidden'>
+      <div className='h-1/4  flex w-full bg-slate-950'> <GoArrowLeft onClick={refreshPage} className='text-4xl ml-1  mt-20 bg-slate-950' /> 
        <Chatuser/>
      
       </div>
-    <div  className='tan py-2  overflow-y-auto ' style={{maxHeight:'calc(88vh - 20vh)'}} >
+    <div  className='tan h-3/4 py-2  overflow-y-auto ' >
 
     <Messages/>
     </div>
+    <div className='h-1/5'>
     <Type/>
+    </div>
     </div>
     </>  )}
     </div>
